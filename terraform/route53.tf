324fee06c94196692a6caba0bf0c9061cd0ffcaf
Herivelto Junior <herijr@gmail.com>
2022-05-25 18:58:13 -0300

    adicionadas configuracoes de ssl

--- terraform/route53.tf
+++ terraform/route53.tf
@@ -16,3 +16,19 @@ resource "aws_route53_record" "website" {
   }
 }
 
+resource "aws_route53_record" "cert_validation" {
+  for_each = {
+    for dvo in aws_acm_certificate.this[0].domain_validation_options : dvo.domain_name => {
+      name   = dvo.resource_record_name
+      record = dvo.resource_record_value
+      type   = dvo.resource_record_type
+    }
+  }
+
+  allow_overwrite = true
+  name = each.value.name
+  records = [each.value.record]
+  ttl = 60
+  type = each.value.type
+  zone_id = data.aws_route53_zone.this[0].zone_id
+}
